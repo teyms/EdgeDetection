a0cfe5b6c63ae17c97047dcb2e8f78389ee97261
teyms <teymingsheng@gmail.com>
2025-01-31 00:17:51 +0800

    Create Unit Test for methods inside Models and Validators folder

---
+++ EdgeDetection.Tests/Models/SobelEdgeDetectionTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Drawing;
+using System.IO;
+using Xunit;
+using EdgeDetection;
+
+namespace EdgeDetection.Tests
+{
+
+    public class SobelEdgeDetectionTests
+    {
+        private static readonly string filePath = "D:/EdgeDetection/EdgeDetection.Tests/images";
+
+        [Theory]
+        [InlineData("/grayscale_apple.jpeg")]
+        [InlineData("/grayscale_fruit.jpg")]
+        [InlineData("/grayscale_lena.png")]
+        public void ProcessEdgeDetection_ShouldProcessImage_WithoutErrors(string input)
+        {
+            string fullPath = filePath + input;
+            Bitmap inputImage = new Bitmap(fullPath);
+            SobelEdgeDetection edgeDetection = new SobelEdgeDetection();
+
+            // Act
+            Bitmap resultImage = edgeDetection.ProcessEdgeDetection(inputImage);
+
+            // Assert
+            Assert.NotNull(resultImage);
+            Assert.Equal(inputImage.Width, resultImage.Width);
+            Assert.Equal(inputImage.Height, resultImage.Height);
+        }
+    }
+
+
+
+}
---
+++ EdgeDetection.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EdgeDetection.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
